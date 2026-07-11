c5a089a27f947d21ca745088873d9815841155ae
ToyHub <toyhub766@gmail.com>
2026-07-11 20:33:17 +0530

    idk

---
+++ frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.toyhub.erp',
+  appName: 'ToyHub ERP',
+  webDir: 'dist'
+};
+
+export default config;
